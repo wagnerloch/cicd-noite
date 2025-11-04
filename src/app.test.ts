d3b80bd5925cf013d32dd2bde01e30365be21aff
Wagner Loch <wagner_loch@hotmail.com>
2025-11-04 20:50:36 -0300

    Atualização do teste

--- src/app.test.ts
+++ src/app.test.ts
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return Hello World', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toEqual('Hello, World!');
+    expect(res.text).toEqual('Olá turma de Engenharia de Software 2! Tudo bem com vocês?');
   });
 });
