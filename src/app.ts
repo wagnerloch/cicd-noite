9f0479abbb92d6c3db8ce70ec3df3d9285970c93
Wagner Loch <wagner_loch@hotmail.com>
2025-11-04 20:45:25 -0300

    Atualização na mensagem inicial

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Olá turma de Engenharia de Software 2!');
+    res.send('Olá turma de Engenharia de Software 2! Tudo bem com vocês?');
 });
 
 export default app;
